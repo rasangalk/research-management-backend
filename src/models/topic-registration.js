be5afe1de2858145f57327b6343e2c3b8f1f200d
rasangalk <rasangalakshith38@gmail.com>
2022-06-03 19:15:24 +0530

    bug fiix

--- src/models/topic-registration.js
+++ src/models/topic-registration.js
@@ -1,61 +1,65 @@
 const mongoose = require("mongoose");
 const topicSchema = new mongoose.Schema(
-  {
-    groupId: {
-      type: String,
-      required: true,
-    },
-    researchInterest: {
-      type: String,
-      required: true,
-    },
+	{
+		groupId: {
+			type: String,
+			required: true,
+		},
+		researchInterest: {
+			type: String,
+			required: true,
+		},
 
-    topic: {
-      type: String,
-      required: true,
-    },
+		topic: {
+			type: String,
+			required: true,
+		},
 
-    supervisor: {
-      type: String,
-      required: true,
-    },
+		supervisor: {
+			type: String,
+			required: true,
+		},
 
-    user: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User",
-      required: true,
-    },
+		user: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: "User",
+			required: true,
+		},
 
-    supervisorName: {
-      type: String,
-      default: "unavailable",
-    },
+		supervisorName: {
+			type: String,
+			default: "unavailable",
+		},
 
-    coSupervisorName: {
-      type: String,
-      default: "unavailable",
-    },
+		coSupervisorName: {
+			type: String,
+			default: "unavailable",
+		},
 
-    coSupervisor: {
-      type: String,
-      default: "unavailable",
-    },
+		coSupervisor: {
+			type: String,
+			default: "unavailable",
+		},
 
-    supervisorStatus: {
-      type: String,
-      default: "unavailable",
-    },
+		supervisorStatus: {
+			type: String,
+			default: "unavailable",
+		},
 
-    coSupervisorStatus: {
-      type: String,
-      default: "unavailable",
-    },
-    supervisorComment: {
-      type: String,
-      default: "unavailable",
-    },
-  },
-  { timestamps: true }
+		coSupervisorStatus: {
+			type: String,
+			default: "unavailable",
+		},
+		supervisorComment: {
+			type: String,
+			default: "unavailable",
+		},
+		coSupervisorComment: {
+			type: String,
+			default: "unavailable",
+		},
+	},
+	{ timestamps: true },
 );
 
 module.exports = mongoose.model("Topic", topicSchema);
